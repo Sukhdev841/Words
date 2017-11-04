d2907545e8ef949fac64827bf07391ba40256378
Sukhdev841 <singhsukhdev1415@gmail.com>
2017-11-04 10:47:55 +0530

    reading new,update,delete

--- words/urls.py
+++ words/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
 	url(r'^all-words-page/$',views.all_words_page,name="all_words_page"),
 	url(r'^all-quotes/$',views.all_quotes_page,name="all_quotes_page"),
-	#url(r'^all-readings/$',views.all_readings_page,name="all_readings_page"),
+	url(r'^all-readings/$',views.all_readings_page,name="all_readings_page"),
 
 
 	######################################################
@@ -21,30 +21,32 @@ urlpatterns = [
 
 	url(r'^new-word-page/$',views.new_word_page,name="new_word_page"),
 	url(r'^new-quote-page/$',views.new_quote_page,name = "new_quote_page"),
-	#url(r'^new-reading/$',views.new_reading_page,name="new_reading_page"),
-	url(r'^update-word-page/(?P<word>\w+)/$',views.update_word_page,name="update_word_page"),
-	url(r'^update-quote-page/(?P<quote>\w+)/$',views.update_quote_page,name="update_quote_page"),
+	url(r'^new-reading/$',views.new_reading_page,name="new_reading_page"),
+
+	url(r'^update-word-page/(?P<word_id>\w+)/$',views.update_word_page,name="update_word_page"),
+	url(r'^update-quote-page/(?P<quote_id>\w+)/$',views.update_quote_page,name="update_quote_page"),
+	url(r'^update-reading-page/(?P<id>\w+)/$',views.update_reading_page,name="update_reading_page"),
 
 
 	##########################################################
 	# saving data from new insertion pages
 	url(r'^save-word/$',views.save_word,name="save_word"),
 	url(r'^save-quote/$',views.save_quote,name="save_quote"),
-	#url(r'^save-reading/$',views.save_reading,name="save_reading"),
+	url(r'^save-reading/$',views.save_reading,name="save_reading"),
 
 	#########################################################
 	##     updating data pages
 
 	url(r'^update-word/(?P<word>\w+)/$',views.update_word,name="update_word"),
-	url(r'^update-quote/(?P<quote>\w+)/$',views.update_quote,name="update_quote"),
-	#url(r'^update-reading/(?P<reading_id>\w+)/$',views.update_reading,name="update_reading"),
+	url(r'^update-quote/(?P<id>\w+)/$',views.update_quote,name="update_quote"),
+	url(r'^update-reading/(?P<id>\w+)/$',views.update_reading,name="update_reading"),
 
 	##########################################################
 	##        deleting data pages
 
 	url(r'^delete-word/(?P<word>\w+)/$',views.delete_word,name="delete_word"),
-	url(r'^delete-quote/(?P<quote>\w+)/$',views.delete_quote,name="delete_quote"),
-	#url(r'^delete-reading/(?P<reading_id>\w+)/$',views.delete_reading,name="delete_reading"),
+	url(r'^delete-quote/(?P<id>\w+)/$',views.delete_quote,name="delete_quote"),
+	url(r'^delete-reading/(?P<id>\w+)/$',views.delete_reading,name="delete_reading"),
 
 
 
@@ -52,5 +54,11 @@ urlpatterns = [
 	# 							other urls
 
 	url(r'^search/$',views.search,name="search"),
+	url(r'^full_reading/(?P<id>\w+)/$',views.full_reading,name="full_reading"),
+
+
+	############################################################
+	##############                Test urls
+	url(r'^test/(?P<id>\w+)/$',views.test,name="test"),
 
 ]
--- words/views.py
+++ words/views.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+
 from .models import Word
 from .models import Quote
+from .models import Reading
 
 from .forms import WordForm
 from .forms import SearchForm
@@ -70,25 +72,48 @@ def all_quotes_page(request,message=None):
 	else:
 		return render(request,'words/show_words.html',{"list":all_quotes,'show_message':True,'message':message,'quotes':True,'page_header':'All Quotes'})
 
+def all_readings_page(request,message=None):
+	all_readings = Reading.objects.all()
+	if message is None:
+		return render(request,'words/show_words.html',{"list":all_readings,'reading':True,'page_header':'All Readings'})
+	else:
+		return render(request,'words/show_words.html',{"list":all_readings,'reading':True,'show_message':True,'message':message})
 
-def update_word_page(request,word):
-	print (word + "is parameter to delete_word view")
+def update_word_page(request,word_id):
+	word_x = Word.objects.get(id=word_id)
+	print (word_x.word + "is parameter to delete_word view")
 	try:
-		word_obj = Word.objects.get(word=word)
+		word_obj = Word.objects.get(id=word_id)
 		return render(request,'words/new_word.html',{"word":word_obj.word,"meaning":word_obj.meaning,
 													"sentence":word_obj.sentence,"punjabi":word_obj.punjabi,
 													"hindi":word_obj.hindi,"tags":word_obj.tags,'edit_word':True})
 	except:
 		return render(request,'words/new_word.html',{'show_message':True,"message":'word not found',"edit_word":True})
 
-def update_quote_page(request,quote):
-	print (quote + " is found ")
+def update_quote_page(request,quote_id):
+	print (Quote.objects.get(id=quote_id).quote + " is found ")
 	try:
-		quote_obj = Quote.objects.get(quote=quote)
-		return render(request,'words/new_word.html',{'quote':quote_obj.quote,'author':quote_obj.author,'language':quote_obj.language,
+		quote_obj = Quote.objects.get(id=quote_id)
+		return render(request,'words/new_word.html',{'id':quote_obj.id,'quote':quote_obj.quote,'author':quote_obj.author,'language':quote_obj.language,
 						'tags':quote_obj.tags,'edit_quote':True})
 	except:
 		return render(request,'words/new_word.html',{'show_message':True,"message":'quote not found',"edit_quote":True})
+
+def update_reading_page(request,id):
+	#r_o = Reading.objects.get(id=id)
+	#print(r_o+ " is passage id")
+	print(" id of reading is "+id)
+	try:
+		print("in try block of update_reading_page ")
+		reading_obj = Reading.objects.filter(id=id)
+		print("object is found in update_reading_page "+reading_obj[0].author)
+		
+		return render(request,'words/new_word.html',{'id':reading_obj[0].id,'title':reading_obj[0].heading,'passage':reading_obj[0].passage,
+						'author':reading_obj[0].author,'tags':reading_obj[0].tags,'language':reading_obj[0].language,'edit_reading':True})
+	except Exception as e:
+		print '%s (%s)' % (e.message, type(e))
+		print ("in exception block of update_reading_page")
+		return render(request,'words/new_word.html',{'show_message':True,'message':"Reading not found"})
 
 
 
@@ -154,7 +179,19 @@ def save_quote(request):
 			print("form is invalid")
 			return render(request,'words/new_word.html',{'show_message':True,'message':'Saving Failed','new_quote':True})
 
-
+def save_reading(request):
+	if request.method == 'POST':
+		title = request.POST.get('title')
+		passage = request.POST.get('passage')
+		author = request.POST.get('author')
+		tags = request.POST.get('tags')
+		language = request.POST.get('language')
+		reading_obj = Reading(heading=title,passage=passage,author=author,tags=tags,language=language)
+		reading_obj.save()
+		print(" new reading is saved successfully ")
+		return main_page(request)
+	else :
+		return main_page(request)
 
 # deletion views
 
@@ -164,11 +201,15 @@ def delete_word(request,word):
 	all_words = get_all_words()
 	return render(request,'words/show_words.html',{'list':all_words,'show_message':True,'message':word+' successfully Deleted', 'words':True, 'page_header':'All Words'})
 	
-
-def delete_quote(request,quote):
-	Quote.objects.filter(quote=quote).delete()
+def delete_quote(request,id):
+	Quote.objects.filter(id=id).delete()
 	all_quotes = get_all_quotes()
-	return render(request,'words/show_words.html',{'list':all_quotes,'show_message':True,'message':quote+' successfully Deleted', 'quotes':True, 'page_header':'All Quotes'})
+	return render(request,'words/show_words.html',{'list':all_quotes,'show_message':True,'message':id+' quote successfully Deleted', 'quotes':True, 'page_header':'All Quotes'})
+
+def delete_reading(request,id):
+	Reading.objects.filter(id=id).delete()
+	all_readings = Reading.objects.all()
+	return render(request,'words/show_words.html',{'list':all_readings,'show_message':True,'message':id+' reading succefully deleted','readings':True,'page_header':'All Readings'})
 
 # updation views
 
@@ -178,15 +219,16 @@ def update_word(request,word):
 
 		form = WordForm(request.POST)
 		if len(form["word"].value()) is not 0:
-			delete_word(request,word);
+			#delete_word(request,word);
 			word = request.POST.get('word')
 			meaning = request.POST.get('meaning','')
 			sentence = request.POST.get('sentence','')
 			hindi = request.POST.get('hindi','')
 			punjabi = request.POST.get('punjabi','')
 			tags = request.POST.get('tags','')
-			word_obj = Word(word = word,meaning = meaning, sentence = sentence,hindi=hindi,punjabi=punjabi,tags=tags)
-			word_obj.save()
+			Word.objects.filter(word=word).update(meaning = meaning, sentence = sentence,hindi=hindi,punjabi=punjabi,tags=tags)
+			word_obj = Word.objects.get(word = word)
+			#word_obj.save()
 			print(word+" is successfully updated in database.")
 			return render(request,'words/new_word.html',{"word":word_obj.word,"meaning":word_obj.meaning,"hindi":word_obj.hindi,"punjabi":word_obj.punjabi,
 													"sentence":word_obj.sentence,"tags":word_obj.tags,'show_message':True,'message':'Sucessfully Updated','edit_word':True})
@@ -195,30 +237,47 @@ def update_word(request,word):
 			print("form is invalid")
 			return render(request,'words/new_word.html',{'show_message':True,'message':'Updation Failed','edit_word':True})
 
-def update_quote(request,quote):
+def update_quote(request,id):
 	if request.method == 'POST':
 
 		form = QuoteForm(request.POST)
 		if len(form["quote"].value()) is not 0:
-			delete_quote(request,quote);
 			quote = request.POST.get('quote')
 			author = request.POST.get('author','')
 			tags = request.POST.get('tags','')
 			language = request.POST.get('language','')
-			quote_obj = Quote(quote= quote,author=author,tags=tags,language=language)
-			quote_obj.save()
+			print(quote+" "+author+" "+language)
+			Quote.objects.filter(id=id).update(quote= quote,author=author,tags=tags,language=language)
+			quote_obj = Quote.objects.get(id=id)
 			
-			return render(request,'words/new_word.html',{'quote':quote_obj.quote,'author':quote_obj.author,
+			#return main_page(request)
+			return render(request,'words/new_word.html',{'id':quote_obj.id,'quote':quote_obj.quote,'author':quote_obj.author,
 									'language':quote_obj.language,'tags':quote_obj.tags,'show_message':True,'message':'Sucessfully Updated','edit_quote':True})
 
 		else:
 			print("form is invalid")
+			return main_page(request)
 			return render(request,'words/new_word.html',{'show_message':True,'message':'Updation Failed','edit_quote':True})
+
+def update_reading(request,id):
+	if request.method == 'POST':
+		heading = request.POST.get('title',' ')
+		author = request.POST.get('author',' ')
+		tags = request.POST.get('tags',' ')
+		language = request.POST.get('language',' ')
+		passage = request.POST.get('passage',' ')
+		Reading.objects.filter(id=id).update(heading=heading,author=author,tags=tags,language=language,passage=passage)
+		reading_obj = Reading.objects.get(id=id)
+		return render(request,'words/new_word.html',{'id':reading_obj.id,'title':reading_obj.heading,'passage':reading_obj.passage,
+						'author':reading_obj.author,'tags':reading_obj.tags,'language':reading_obj.language,'edit_reading':True,'show_message':True,'message':"Reading successfully updated."})
 
 
 
 # other views
 
+def full_reading(request,id):
+	read = Reading.objects.get(id=id)
+	return render(request,'words/reading.html',{"obj":read})
 
 # for searching
 def search(request):
@@ -257,3 +316,11 @@ def get_all_words():
 def get_all_quotes():
 	return Quote.objects.all()
 
+
+###########################################################
+########              test views
+
+def test(request,id):
+	print (id + " is id of quote")
+	print("came in test view")
+	return main_page(request)
